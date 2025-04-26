9ce65c5869b6d3a39a839671d1f2380176605951
Miguel Angelo <miguelrsant01@gmail.com>
2025-04-26 13:49:32 +0000

    Adição da mensagem de carinho

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
     return (
     <>
-        <h1>Clone TabNews</h1>
+        <p>Fala Gui Fimezaa! Cara te considero muito, sei que já vacilei várias vezes mas tenho expectativa que nossa amizada vai durar muito tempo e ficar cada vez mais forte!!!</p>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStlfv_8n-DCOlkO7Q6RCeKDHtuJlR18An14Q&s" />
+        <h3>Eu, você e o Luis no <strong>FUTURO!!</strong></h3>
     </>
     )
 }
