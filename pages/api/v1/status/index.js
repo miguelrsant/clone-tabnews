bbdd7dee2fc125b32dde0785987f7e24832fedb6
Miguel Angelo <miguelrsant01@gmail.com>
2025-05-03 12:45:02 +0000

    add database.js and .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(req, res) {
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1+1;");
+  console.log(result.rows);
   res.status(200).json({ chave: "quero dinheiro é" });
 }
