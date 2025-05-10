c96510258643dbf4f4700758b8581ee35758d6e3
Miguel Angelo <miguelrsant01@gmail.com>
2025-05-10 11:37:51 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,34 @@
 import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "quero dinheiro é" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxconnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxconnectionsValue =
+    databaseMaxconnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedconnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedconnectionsValue =
+    databaseOpenedconnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxconnectionsValue),
+        opened_connections: databaseOpenedconnectionsValue,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const responseBody = await res.json();
+
+  const parsedUpdateAT = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAT);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
